89b2e25a59281f32b7b13115dcb8b58550997e58
jen <chenchev10@gmail.com>
2026-01-03 18:01:49 -0800

    MONGO URI

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import userRoutes from "./routes/user.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.MONGO_URI || 5000;
 
 app.use(cors());
 app.use(express.json());
